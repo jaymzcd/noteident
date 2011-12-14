8714e910e8d7a317899fa54ee0d7184d833e8fa5
jaymz <jaymz@u-dox.com>
2011-12-14 02:58:35 +0000

    tabs and some more hps work added

--- README.md
+++ README.md
@@ -1,7 +1,34 @@
-# Note Identification
+# Note Identification of musical instruments via Spectral Analysis
+
+## Introduction
+
+This is an old hobby of mine from university. I'm trying to identify at least
+*one* note from a guitar solo over time from a recording. Using FFTs and spectral
+analysis I assume something is rougly possible. By using some knoweldge about
+"expected" frequencies and some statistics.
+
+It's also a good way to learn pylab some more (matplotlib/numpy) and get used to
+using it for numerical work in combination with Sage. The idea/work that I
+did to remind myself of the interest in this is: http://goo.gl/RtWhn
+
+## Setup / Requirements
 
 Use [audiolab](https://github.com/cournape/audiolab) to read in ogg files to a
 numpy array. Then plot specgram.
 
 http://matplotlib.sourceforge.net/api/mlab_api.html#matplotlib.mlab.specgram
 http://www.ar.media.kyoto-u.ac.jp/members/david/softwares/audiolab/sphinx/index.html
+
+The specgram plot will return a tuple consiting of ["periodograms"](http://en.wikipedia.org/wiki/Periodogram),
+the frequency bins and the time axis data for the plot. Within the lab session
+the individual PSDs can be plotted for example:
+
+## Good resources
+
+  * [Note frequencies](http://www.vaughns-1-pagers.com/music/musical-note-frequencies.htm)
+  * [Ranges of instruments to a piano](http://www.bosendorfer-audio.co.uk/frequencies_of_music.html)
+
+
+## Sample
+
+Sample matches tab at section starting line 699.
--- lab_session.py
+++ lab_session.py
@@ -1,37 +1,83 @@
+import sys
 import matplotlib.pyplot as plt
-from numpy import linspace
+
+from matplotlib import cm
+from numpy import linspace, multiply
 from scikits.audiolab import Sndfile, play
 
-def get_ogg(f='src/orion.ogg'):
+def get_ogg(f='src/one.ogg'):
     return Sndfile(f)
-
-def plot_frames(n=1024):
-    y = orion.read_frames(n)
-    plt.plot(y)
-    return y
 
 def snd_length(snd):
     return float(snd.nframes) / snd.samplerate
 
-def spec_plot(snd, t=1):
-    frame_cnt = snd.samplerate * t
-    length = snd_length(snd)
+def spec_plot(snd, start=0, end=1, fft_size=256):
+    total_length = snd_length(snd)
+    sample_length = (end - start)
+
+    frame_cnt = snd.samplerate * sample_length
+    seek_frame = snd.samplerate * start
+
+    if seek_frame:
+        snd.seek(seek_frame)
 
     samples = snd.read_frames(frame_cnt)
-
     left_channel = samples[:, 0]
-    print "Sound length: %f. Reading %d samples (%ds)" % (length, frame_cnt, t)
 
-    time = linspace(0, t, snd.samplerate*t)
+    print "Sound length: %f. Reading %d samples (%ds)" % \
+        (total_length, frame_cnt, sample_length)
+
+    time = linspace(start, end, frame_cnt)
+
     plt.subplot(211)
-    fft_size = 512
-    plt.specgram(left_channel, NFFT=fft_size, xextent=(0, t))
+    spec = plt.specgram(left_channel, NFFT=fft_size,
+        xextent=(start, end), cmap=cm.Spectral, Fs=snd.samplerate,
+        noverlap=256, pad_to=2048)
+    plt.title('Spectrogram [%d samples]' % frame_cnt)
+
     plt.subplot(212)
     plt.plot(time, left_channel)
+    plt.title('Time series (%0.2fs)' % sample_length)
+
     plt.show()
-    return (time, left_channel)
+
+    return (time, left_channel, spec)
+
+def get_psd(spec, freq):
+    """
+        Extracts the right PSD from the specgram output
+    """
+    f_index = len([1 for f in spec[1] if f < freq]) - 1
+    psd = (spec[2], spec[0][f_index, :])
+    print "Wanted: %sHz, got %sHz [index %s]" % (freq, spec[1][f_index], f_index)
+    return ((freq, f_index, spec[1][f_index]), psd)
+
+def hps(spec, base_freq, harmonics):
+    freqs = [base_freq*h for h in range(1, harmonics + 1)]
+    psds = list()
+    psd_freqs = list()
+    for freq in freqs:
+        psd_data = get_psd(spec, freq)
+        psds.append(psd_data[1][1])
+        psd_freqs.append(psd_data[0])
+    psds.insert(0, reduce(multiply, psds))
+    return psds, psd_freqs
 
 if __name__=='__main__':
+    try:
+        # (Expect to run this from *within* ipython session!)
+        start = float(sys.argv[1])
+        end = float(sys.argv[2])
+        fft_size = float(sys.argv[3])
+    except IndexError:
+        start = 0
+        end = 2
+        fft_size = 512
+
     snd = get_ogg()
-    snd_samples = spec_plot(snd, t=2.5)
+    snd_samples = spec_plot(snd, start=start, end=end, fft_size=fft_size)
+    snd.close()
+    spec = snd_samples[2]
+    psds = hps(spec, 440, 3)
+
 
