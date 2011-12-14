7277ee9f255c1d06bd2167bd82852823f2a23031
jaymz <jaymz@u-dox.com>
2011-12-14 11:44:53 +0000

    plot individual spectra from specgram

--- lab_session.py
+++ lab_session.py
@@ -3,12 +3,23 @@ from pylab import *
 from scikits.audiolab import Sndfile, play
 
 def get_ogg(f='src/emajor_piano.ogg'):
+    """
+        Wrapper to return our ogg file using a default sound
+    """
     return Sndfile(f)
 
 def snd_length(snd):
+    """
+        Total length in seconds for our given sound file
+    """
     return float(snd.nframes) / snd.samplerate
 
 def spec_plot(snd, start=0, end=1, fft_size=512):
+    """
+        Create a spectrogram, FFT & time series plot of the given sound between
+        start & end seconds. Frequency response is limited to the postive
+        frequencies up to half the sounds sample rate
+    """
     total_length = snd_length(snd)
     sample_length = (end - start)
 
@@ -52,11 +63,12 @@ def spec_plot(snd, start=0, end=1, fft_size=512):
 
     show()
 
-    return (time, left_channel, spec)
+    return (time, left_channel, spec, (fscale, sig_ft))
 
 def get_psd(spec, freq):
     """
-        Extracts the right PSD from the specgram output
+        Extracts the right PSD from the specgram output - for a given requested
+        frequency the closest matching one is returned from the specgram plots.
     """
     f_index = len([1 for f in spec[1] if f < freq]) - 1
     psd = (spec[2], spec[0][f_index, :])
@@ -64,6 +76,11 @@ def get_psd(spec, freq):
     return ((freq, f_index, spec[1][f_index]), psd)
 
 def hps(spec, base_freq, harmonics):
+    """
+        Build a harmonic product spectrum for a base frequency - grab the
+        PSDs of the harmonics and combine into a single spectrum to identify
+        the strongest harmonic bases for the signals time length
+    """
     freqs = [base_freq*h for h in range(1, harmonics + 1)]
     psds = list()
     psd_freqs = list()
@@ -73,6 +90,14 @@ def hps(spec, base_freq, harmonics):
         psd_freqs.append(psd_data[0])
     psds.insert(0, reduce(multiply, psds))
     return psds, psd_freqs
+
+def plot_spectra(spec, max_findex, skip=10):
+    leg = list()
+    print "Plotting up to max %0.2fHz" % (spec[1][max_findex])
+    for x in range(0, max_findex, skip):
+        plot(spec[2], spec[0][x])
+        leg.append(spec[1][x])
+    legend(leg)
 
 if __name__=='__main__':
     try:
@@ -85,6 +110,7 @@ if __name__=='__main__':
         end = 2
         fft_size = 512
 
+    # Sample session - load our ogg, plot specs and calculate the PSDs
     snd = get_ogg()
     snd_samples = spec_plot(snd, start=start, end=end, fft_size=fft_size)
     snd.close()
