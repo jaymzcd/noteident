915709ffb9ef137d50b97dd641cdca79bd6b1037
jaymz <jaymz@u-dox.com>
2011-12-14 11:01:07 +0000

    some fft stuff - always forget this!

---
+++ fft_helpers.py
@@ -0,0 +1,30 @@
+from scipy import *
+from pylab import *
+
+def fft_plot():
+    f = 3 # hz
+    fs = 200.0 # sampling freq
+
+    L = 128
+
+    x = arange(0, L, 1/fs)
+    y = 5*sin(2*pi*f*x) + sin(2*pi*f*3*x) + 10*sin(2*pi*(f+3)*5*x)
+
+    fmax = fs / 2
+
+    N = 1024
+    yft = fft(y, N)
+    faxis = linspace(-fmax, fmax, N)
+
+    yshift = fftshift(abs(yft))
+
+    return (x, y, faxis, yshift)
+
+def plot_stuff(x, y, fx, ft):
+    subplot(211)
+    plot(x, y)
+    subplot(212)
+    mid = len(fx)/2
+    plot(fx[mid:], ft[mid:])
+
+
